d3782ad5d959900101d1654c408cc0493b568804
Lok/Tom Chow <lohchness@gmail.com>
2025-04-18 19:32:19 +1000

    Create segmentshape between each pair for every substroke

--- stroke.gd
+++ stroke.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends StaticBody2D
 class_name Stroke
 
 ## A stroke is made up of Vector2s
@@ -6,10 +6,20 @@ class_name Stroke
 ## Simple straight, Simple curve, Complex straight, Complex curve
 ## Has a direction: Compass directions (straight) or CW/CCW (curve)
 
-var stroke_type
-
 var stroke_points: Array[Array] = [[]]
+var stroke_type
 
 func create_stroke(substrokes: Array[Array]):
 	stroke_points = substrokes
 	
+	# For every pair,
+	# Create a collision shape 2d with the shape a SegmentShape2D
+	# Shape.A is the first vector, Shape.B is the second vector
+	for i in range(len(stroke_points)):
+		for j in range(len(stroke_points[i]) - 1):
+			var collisionshape = CollisionShape2D.new()
+			var shape = SegmentShape2D.new()
+			shape.a = stroke_points[i][j]
+			shape.b = stroke_points[i][j+1]
+			collisionshape.shape = shape
+			add_child(collisionshape)
