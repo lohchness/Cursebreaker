3e0e992b69313b1a74cd10e3c37caa3308ffa5d0
Lok/Tom Chow <lohchness@gmail.com>
2025-04-20 23:27:43 +1000

    crate level script

---
+++ scripts/level.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+# Level manages the map and its marker(s):
+#  - Moves the map>marker ghost to its position
+#  - Moves the map>marker actual to marker ghost?
+# Holds data for all the targets the marker actual has to hit
+# Holds level name
