f83584bcb403c3b9af035f3e0597704d34ed2b3e
Lok/Tom Chow <lohchness@gmail.com>
2025-04-21 11:24:08 +1000

    remove input

--- scripts/map.gd
+++ scripts/map.gd
@@ -4,7 +4,3 @@ extends Node2D
 
 func move_marker_actual(x, y):
 	marker_actual.move_to_dest(Vector2(x,y))
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("incant"):
-		move_marker_actual(253, -115)
