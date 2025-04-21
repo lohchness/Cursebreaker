9183c699f7871d91ffa3d5fdbef92f37f49bb717
Lok/Tom Chow <lohchness@gmail.com>
2025-04-21 18:41:44 +1000

    smooth transition between menu and level select

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -1,1 +1,15 @@
 extends Control
+
+
+func _ready() -> void:
+	modulate = Color(0, 0, 0, 255)
+
+func _physics_process(delta: float) -> void:
+	if modulate.r < 1:
+		modulate.r += delta
+		modulate.g += delta
+		modulate.b += delta
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -11,10 +11,14 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if move:
 		position += Vector2(0,-500) * delta
-		modulate = lerp(modulate, Color(origcolor.r, origcolor.g, origcolor.b, 0), 5 * delta)
+		#modulate = lerp(modulate, Color(origcolor.r, origcolor.g, origcolor.b, 0), 5 * delta)
+		#modulate.a -= delta
+		modulate.r -= delta
+		modulate.g -= delta
+		modulate.b -= delta
 	
-	if modulate == Color(255, 255, 255, 0):
-		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	if modulate.r <= 0:
+		get_tree().change_scene_to_file("res://scenes/level_select.tscn")
 
 
 func _on_texture_button_pressed() -> void:
