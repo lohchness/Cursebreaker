ef21d67e7b6bd9797214685188a343004e7b3cee
Lok/Tom Chow <lohchness@gmail.com>
2025-04-22 08:52:52 +1000

    Connect levels to level select

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -17,3 +17,12 @@ func _on_back_pressed() -> void:
 
 func _on_tutorial_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/tutorial_page_1.tscn")
+
+func _on_level_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+
+func _on_level_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Level2.tscn")
+
+func _on_level_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Level3.tscn")
