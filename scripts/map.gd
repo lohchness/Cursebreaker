cd473d0a0937068e690703771c1c64adc4348444
Lok/Tom Chow <lohchness@gmail.com>
2025-04-21 11:22:22 +1000

    Move marker a given distance at constant speed

--- scripts/map.gd
+++ scripts/map.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
+@onready var marker_actual = $MarkerActual
 
 func move_marker_actual(x, y):
-	
-	pass
+	marker_actual.move_to_dest(Vector2(x,y))
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("incant"):
+		move_marker_actual(253, -115)
--- scripts/marker_actual.gd
+++ scripts/marker_actual.gd
@@ -1,4 +1,24 @@
 extends CharacterBody2D
 
+var speed = 200
+
+var direction = Vector2(0,0)
+var remaining_distance = 0
+
+
 func _physics_process(delta: float) -> void:
+	if remaining_distance > 0:
+		velocity = direction * speed
+		remaining_distance -= speed * delta
+	else:
+		velocity = Vector2.ZERO
+	
 	move_and_slide()
+
+func move_to_dest(dest: Vector2):
+	# Given a Vector2 destination:
+	# Calculate distance from current position to destination
+	# Calculate direction
+	
+	remaining_distance = dest.length()
+	direction = dest.normalized()
